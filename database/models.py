30d5484dca1ccf6d059c88edde829db13945ec21
knowbee <knowbeeinc@gmail.com>
2022-01-26 12:36:19 +0200

    Changes DataFields types

--- database/models.py
+++ database/models.py
@@ -5,11 +5,11 @@ class Location(db.Model):
     __tablename__ = "locations"
 
     id = db.Column(db.Integer, primary_key=True)
-    latitude = db.Column(db.String(20), unique=True, nullable=False)
-    longitude = db.Column(db.String(20), unique=True, nullable=False)
-    street_name = db.Column(db.String(120), unique=True, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+    street_name = db.Column(db.String(120), nullable=False)
 
-    def __init__(self, latitude, longitude, street_name):
+    def __init__(self, latitude: float, longitude: float, street_name: str):
         self.latitude = latitude
         self.longitude = longitude
         self.street_name = street_name
